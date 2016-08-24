ec2fec22824b8b7662569f9a905f8f524b816780
Yuuta Sumioka <ysumio120@yahoo.com>
2016-08-24 05:43:57 -0400

    Replaced placeholders with actual images; Click event for choosing character and first opponent

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,66 @@
 /*
-	HP:  Health Points
-	AP:  Attack Power
-	cAP: Counter Attack Power
+	HP:  	Health Points
+	AP:  	Attack Power
+	cAP: 	Counter Attack Power
+	attack: Increases AP by base amount every attack
 */
 var obiWan = {
-	HP: 50,
-	AP:50,
-	cAP: 50,
+	HP: 100,
+	AP: 10,
+	cAP: 15,
 	attack: function() {
 		this.AP += this.AP;
 	}
 };
 
 var darthVader = {
-	HP: 50,
-	AP: 50,
-	cAP: 50,
+	HP: 150,
+	AP: 15,
+	cAP: 20,
 	attack: function() {
 		this.AP += this.AP;
 	}	
 };
 
-var lukeSkywalker = {
-	HP: 50,
-	AP: 50,
-	cAP: 50,
+var hanSolo = {
+	HP: 90,
+	AP: 5,
+	cAP: 8,
 	attack: function() {
 		this.AP += this.AP;
 	}
 };
 
 var chewbacca = {
-	HP: 50,
-	AP: 50,
-	cAP: 50,
+	HP: 300,
+	AP: 8,
+	cAP: 15,
 	attack: function() {
 		this.AP += this.AP;
 	}
 };
 
-var bool = true;
+var ally = true;
+var enemy = true;
+var originalPosition;
+var enemyChosen;
+
 $(document).ready(function() {
-	if(bool) {
-		$(".char").on("click", function() {
-			var picked = $(this).appendTo(".ally");
-			bool = false;
-			$(".selection .char").appendTo(".enemies");
+		$(".char div").on("click", function() {
+			console.log(originalPosition);
+			if(ally) {
+				$(this).appendTo(".ally");
+				ally = false;
+			}
+			else if(enemy) {
+				enemyChosen = $(this);
+				originalPosition = $(this).parent();
+				$(this).appendTo(".opponent");
+				enemy = false;
+				//console.log("REACHED");
+			}
 		});
-	}
+
+		
 
 });
